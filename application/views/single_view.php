75994deaa375362579df970afe79cddddb349ced
Ben Adrian Sarmiento <me@bensarmiento.com>
2013-04-12 10:52:47 +0800

    added about page links

--- application/views/single_view.php
+++ application/views/single_view.php
@@ -38,7 +38,7 @@
                     <div class="nav-collapse collapse">
                         <ul class="nav">
                             <li><a href="/movies/browse">Browse</a></li>
-                            <li><a href="#about">About</a></li>
+                            <li><a href="/misc/about">About</a></li>
                             <li><a href="#contact">Contact</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
