5d0f326e4362f96e435b8752e77eee5912dda782
Ben Adrian Sarmiento <me@bensarmiento.com>
2013-04-12 10:50:54 +0800

    added about page

---
+++ application/controllers/misc.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Misc extends CI_Controller {
+
+	private $view_data;
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function index()
+	{
+		//var_dump($view_data);die();
+		$this->load->view('welcome_message', $view_data);
+	}
+
+	public function about()
+	{
+		$view_data['count'] = $this->mongo_db
+		->count('movies');
+		exec("pgrep lighttpd", $pids);
+		if (empty($pids)) {
+			$view_data['d_running'] = false;
+		}
+		else {
+			$view_data['d_running'] = true;
+		}
+
+		$this->load->view('about', $view_data);
+	}
+}
+
+/* End of file misc.php */
+/* Location: ./application/controllers/misc.php */
---
+++ application/views/about.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>WATCH YOUTUBE MOVIES - About Us</title>
+        <meta name="description" content="watch full movies on youtube this site only serves as a directory">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="/static/css/bootstrap.css">
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="/static/css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="/static/css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <script>window.html5 || document.write('<script src="/static/js/html5shiv.js"><\/script>')</script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="/">WATCH YOUTUBE MOVIES</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li><a href="/movies/browse">Browse</a></li>
+                            <li><a href="/misc/about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li class="nav-header">Nav header</li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="span12">
+                    <h2>About Us</h2>
+                    <p>Something something blah blah freedom something</p>
+                    <p>PHP (front-end) --> MongoDB <-- Python (back-end)</p>
+                    <p>Movie count: <?=$count?></p>
+                    <p>Crawler: <?=$d_running?'<span class="label label-success">Running</span>':'<span class="label label-important">Stopped</span>'?></p>
+                </div>
+            </div>
+
+            <hr>
+
+            <footer>
+                <p>&copy; WATCH YOUTUBE MOVIES 2012</p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script>window.jQuery || document.write('<script src="/static/js/vendor/jquery-1.9.1.js"><\/script>')</script>
+
+        <script src="/static/js/bootstrap.js"></script>
+
+        <script src="/static/js/main.js"></script>
+
+        <!--<script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>-->
+    </body>
+</html>
