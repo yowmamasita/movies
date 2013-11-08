f1b5bfa6578f981d2f8c2805e7abbd2a738f7e12
Ben Adrian Sarmiento <me@bensarmiento.com>
2013-11-09 00:51:23 +0800

    added google analytics

--- application/views/header.php
+++ application/views/header.php
@@ -16,6 +16,16 @@
         </style>
         <link rel="stylesheet" href="/static/css/bootstrap-responsive.css">
         <link rel="stylesheet" href="/static/css/main.css">
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-45576489-1', 'movie.jp');
+          ga('send', 'pageview');
+
+        </script>
 
         <!--[if lt IE 9]>
             <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
